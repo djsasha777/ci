ad291aa2052098fe1b47a5fa071d6815e711625b
Alex <djsasha777@mail.ru>
2025-09-10 22:07:47 +0300

    up

--- watcher/main.py
+++ watcher/main.py
@@ -4,10 +4,12 @@ import yaml
 from kubernetes import client, config, watch
 import git
 
+
 repo_url = os.getenv('REPO')  # Пример: https://github.com/djsasha777/infra.git
-branch = os.getenv('BRANCH', 'main')
+branch = os.getenv('BRANCH')
 filePath = os.getenv('FILEPATH')  # Пример: infra/ansible/loadbalancer-haproxy/group_vars/all.yaml
 token = os.getenv('TOKEN')  # GitHub token
+token = os.getenv('EXTERNAL')  # external tag for service exploring
 
 # Настройка URL для доступа с токеном
 def get_auth_repo_url(repo_url, token):
@@ -15,9 +17,11 @@ def get_auth_repo_url(repo_url, token):
         return repo_url.replace("https://", f"https://{token}@")
     return repo_url
 
-def update_haproxy(ingress_name, ingress_ip):
+
+def update_haproxy(ingress_name, ingress_ip, has_issuer):
     repo_path = "/tmp/repo"
     auth_repo_url = get_auth_repo_url(repo_url, token)
+
 
     # Клонирование или обновление репозитория
     if not os.path.exists(repo_path):
@@ -28,51 +32,71 @@ def update_haproxy(ingress_name, ingress_ip):
         repo.git.checkout(branch)
         repo.remotes.origin.pull()
 
+
     values_path = os.path.join(repo_path, filePath)
+
 
     # Загрузка YAML файла
     with open(values_path, 'r') as f:
         data = yaml.safe_load(f) or {}
 
+
     if "haproxySubdomain" not in data or data["haproxySubdomain"] is None:
         data["haproxySubdomain"] = []
+
 
     if "acmeSubdomain" not in data or data["acmeSubdomain"] is None:
         data["acmeSubdomain"] = []
 
+
     # Обновление или добавление новых записей
-    found = False
+    # Флаг для проверки существующих записей для haproxySubdomain и acmeSubdomain
+    found_haproxy = False
+    found_acme = False
+
+
+    # Обновляем haproxySubdomain
     for entry in data["haproxySubdomain"]:
         if entry.get("name") == ingress_name:
             entry["ip"] = ingress_ip
             entry["port"] = 443
             entry["proxy"] = False
-            found = True
+            found_haproxy = True
             break
 
-    for entry in data["acmeSubdomain"]:
-        if entry.get("name") == ingress_name:
-            entry["ip"] = ingress_ip
-            entry["port"] = 80
-            found = True
-            break
 
-    if not found:
+    # Обновляем только если есть cert-manager.io/cluster-issuer аннотация
+    if has_issuer:
+        for entry in data["acmeSubdomain"]:
+            if entry.get("name") == ingress_name:
+                entry["ip"] = ingress_ip
+                entry["port"] = 80
+                found_acme = True
+                break
+
+
+    # Если записи не найдены, добавляем новые
+    if not found_haproxy:
         data["haproxySubdomain"].append({
             "name": ingress_name,
             "ip": ingress_ip,
             "port": 443,
             "proxy": False
         })
+
+
+    if has_issuer and not found_acme:
         data["acmeSubdomain"].append({
             "name": ingress_name,
             "ip": ingress_ip,
             "port": 80
         })
 
+
     # Сохранение изменений
     with open(values_path, 'w') as f:
         yaml.dump(data, f, default_flow_style=False)
+
 
     # Коммит и пуш
     repo.git.add(filePath)
@@ -81,31 +105,42 @@ def update_haproxy(ingress_name, ingress_ip):
     origin.push(branch)
     print(f"Git изменения запушены для {ingress_name}")
 
+
 def main():
     print("Обновляем конфигурацию Kubernetes ingresses")
+
 
     # Загружаем конфигурацию внутри кластера
     config.load_incluster_config()
     networking_v1 = client.NetworkingV1Api()
     w = watch.Watch()
 
+
     def scan_existing_ingress():
         ingresses = networking_v1.list_ingress_for_all_namespaces().items
         for ingress in ingresses:
             annotations = ingress.metadata.annotations or {}
 
-            if annotations.get('kubernetes.io/ingress.class') == 'nginx' and annotations.get('cert-manager.io/cluster-issuer') == 'letsencrypt-production':
+
+            if annotations.get('kubernetes.io/ingress.class') == 'nginx':
                 name = ingress.metadata.name
+
 
                 ingress_ip = None
                 if ingress.status.load_balancer and ingress.status.load_balancer.ingress:
                     if len(ingress.status.load_balancer.ingress) > 0:
                         ingress_ip = ingress.status.load_balancer.ingress[0].ip
 
-                print(f"Scan existing - Name: {name}, IP: {ingress_ip}")
-                update_haproxy(name, ingress_ip)
+
+                has_issuer = annotations.get('cert-manager.io/cluster-issuer') == "letsencrypt-production"
+
+
+                print(f"Scan existing - Name: {name}, IP: {ingress_ip}, Has issuer: {has_issuer}")
+                update_haproxy(name, ingress_ip, has_issuer)
+
 
     scan_existing_ingress()
+
 
     while True:
         try:
@@ -115,9 +150,10 @@ def main():
                 ingress = event['object']
                 annotations = ingress.metadata.annotations or {}
 
+
                 if (event_type == 'ADDED'
-                    and annotations.get('kubernetes.io/ingress.class') == 'nginx'
-                    and annotations.get('cert-manager.io/cluster-issuer') == 'letsencrypt-production'):
+                    and annotations.get('kubernetes.io/ingress.class') == 'nginx'):
+
 
                     name = ingress.metadata.name
                     ingress_ip = None
@@ -125,12 +161,18 @@ def main():
                         if len(ingress.status.load_balancer.ingress) > 0:
                             ingress_ip = ingress.status.load_balancer.ingress[0].ip
 
-                    print(f"New ingress - Name: {name}, IP: {ingress_ip}")
-                    update_haproxy(name, ingress_ip)
+
+                    has_issuer = annotations.get('cert-manager.io/cluster-issuer') == "letsencrypt-production"
+
+
+                    print(f"New ingress - Name: {name}, IP: {ingress_ip}, Has issuer: {has_issuer}")
+                    update_haproxy(name, ingress_ip, has_issuer)
+
 
         except Exception as e:
             print(f"Ошибка в watch: {e}")
             time.sleep(10)
+
 
 
 if __name__ == "__main__":
